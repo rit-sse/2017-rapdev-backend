21c0dc7bc8bb05b6815c5611475ec28a05ff465a
Carter Buce <cmb9400@rit.edu>
2017-01-28 15:18:51 -0500

    added jwt token api

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 from database import db_session
 import models
 import json
+import jwt
+
+
+
+secret = 'secret'
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -22,4 +27,21 @@ def create():
     db_session.commit()
     return str(models.User.query.all())
 
+@app.route('/api/v1/auth', methods=['POST'])
+def auth():
+    username=request.form['username']
 
+    user = models.User.query.filter_by(name=username).first()
+    
+    if user is None:
+        user = models.User(username, username + '@')
+        db_session.add(user)
+        db_session.commit()
+        
+    encoded = jwt.encode({'id': user.id}, secret, algorithm='HS256') 
+
+    return json.dumps({'token': encoded})
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ run.bat
@@ -0,0 +1,3 @@
+
+C:\python27\python.exe main.py
+pause
